50b9f9bbeb019e8074f0107357575d1c4f12195d
Abhishek Tyagi <abhishektyagi@gmail.com>
2024-06-02 03:22:37 +0530

    done

--- app/Product/page.tsx
+++ app/Product/page.tsx
@@ -1,9 +1,8 @@
-
 import React from "react";
 import styles from "../../lib/styles/Home.module.css";
 import { getCollections } from "@/lib/actions/actions";
 import Link from "next/link";
-import Image from "next/image"; 
+import Image from "next/image";
 
 const Category = async () => {
   const collections = await getCollections();
@@ -11,18 +10,22 @@ const Category = async () => {
   return (
     <section className={styles.slider}>
       <div className={styles.container}>
-        {collections.map((collection: CollectionType) => (
-          <Link href={`/Product/${collection._id}`} key={collection._id}>
-            <Image
-              src={collection.image}
-              alt={collection.title}
-              width={350}
-              height={200}
-              className="rounded-lg cursor-pointer"
-            />
-            <h3 className={styles.catName}>{collection.title}</h3>
-          </Link>
-        ))}
+        <div className={styles.category}>
+          {collections.map((collection: CollectionType) => (
+            <Link href={`/Product/${collection._id}`} key={collection._id} className="nana" style={{textDecoration:'none'}}>
+              <div className={styles.image}>
+                <Image
+                  src={collection.image}
+                  alt={collection.title}
+                  width={350}
+                  height={200}
+                  className="rounded-lg cursor-pointer"
+                />
+              </div>
+              <h3 className={styles.catName}>{collection.title}</h3>
+            </Link>
+          ))}
+        </div>
       </div>
     </section>
   );
