cc440acbaa1e8cdf231915a73981525372b4e89a
Abhishek Tyagi <abhishektyagi@gmail.com>
2024-06-03 10:48:19 +0530

    header tsk is done

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,10 @@
-"use client"
-import React, { useState } from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from 'next/link';
 import Image from 'next/image';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { FaBars, FaTimes } from 'react-icons/fa';
+import { getCollections } from "@/lib/actions/actions";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,6 +12,16 @@ const Header = () => {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+
+  const [collections, setCollections] = useState([]);
+
+  useEffect(() => {
+    const fetchCollections = async () => {
+      const fetchedCollections = await getCollections();
+      setCollections(fetchedCollections.reverse());
+    };
+    fetchCollections();
+  }, []);
 
   return (
     <header className="sticky-top bg-light border-bottom">
@@ -27,10 +38,12 @@ const Header = () => {
         <Navbar.Collapse id="basic-navbar-nav" className={`${isOpen ? 'show' : ''}`}>
           <Nav className="ms-auto">
             <Nav.Link href="/" className="px-2 link-secondary">Home</Nav.Link>
-            <NavDropdown title="Products" id="basic-nav-dropdown" className="px-2 link-dark">
-              <NavDropdown.Item href="#feature1">Feature 1</NavDropdown.Item>
-              <NavDropdown.Item href="#feature2">Feature 2</NavDropdown.Item>
-              <NavDropdown.Item href="#feature3">Feature 3</NavDropdown.Item>
+            <NavDropdown title="Collections" id="basic-nav-dropdown" className="px-2 link-dark">
+              {collections.map((collection: CollectionType) => (
+                <NavDropdown.Item key={collection._id} href={`/Product/${collection._id}`}>
+                  {collection.title}
+                </NavDropdown.Item>
+              ))}
             </NavDropdown>
             <Nav.Link href="/About" className="px-2 link-dark">About</Nav.Link>
             <Nav.Link href="/Contact" className="px-2 link-dark">Contact</Nav.Link>
