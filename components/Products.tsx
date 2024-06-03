c20abf80b200a84dfb618da8fadaae4e3974a567
Abhishek Tyagi <abhishektyagi@gmail.com>
2024-06-03 11:14:31 +0530

    done view all product and all producst page is created

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 import React, { useEffect, useState } from "react";
 import { getProducts } from "@/lib/actions/actions";
 import Link from "next/link";
 import Skeleton from "react-loading-skeleton";
-import 'react-loading-skeleton/dist/skeleton.css';
+import "react-loading-skeleton/dist/skeleton.css";
 
 const ProductSection = () => {
   const [products, setProducts] = useState([]);
@@ -31,68 +31,79 @@ const ProductSection = () => {
           </p>
         </div>
         <div className="row">
-          {loading ? (
-            Array.from({ length: 4 }).map((_, index) => (
-              <div className="col-md-3 mb-4" key={index}>
-                <div className="card h-100 border-0">
-                  <div
-                    className="card-img-top d-flex justify-content-center align-items-center"
-                    style={{
-                      height: "230px",
-                      overflow: "hidden",
-                      position: "relative",
-                    }}
-                  >
-                    <Skeleton width="100%" height="100%" />
-                  </div>
-                  <div className="card-body d-flex flex-column">
-                    <h5 className="card-title text-center mb-4">
-                      <Skeleton width="60%" />
-                    </h5>
-                    <div className="mt-auto text-center">
-                      <Skeleton width="100px" height="40px" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))
-          ) : (
-            topProducts.map((product: ProductType) => (
-              <div className="col-md-3 mb-4" key={product._id}>
-                <div className="card h-100 border-0">
-                  <div
-                    className="card-img-top d-flex justify-content-center align-items-center"
-                    style={{
-                      height: "230px",
-                      overflow: "hidden",
-                      position: "relative",
-                    }}
-                  >
-                    <img
-                      src={product.media[0]}
-                      alt={product.title}
-                      className="img-fluid"
+          {loading
+            ? Array.from({ length: 4 }).map((_, index) => (
+                <div className="col-md-3 mb-4" key={index}>
+                  <div className="card h-100 border-0">
+                    <div
+                      className="card-img-top d-flex justify-content-center align-items-center"
                       style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                        borderRadius: "10px",
-                        transition: "transform 0.5s",
+                        height: "230px",
+                        overflow: "hidden",
+                        position: "relative",
                       }}
-                    />
-                  </div>
-                  <div className="card-body d-flex flex-column">
-                    <h5 className="card-title text-center mb-4">{product.title}</h5>
-                    <div className="mt-auto text-center">
-                      <Link href="/Contact" className="btn btn-success btn-custom">
-                        Contact Us
-                      </Link>
+                    >
+                      <Skeleton width="100%" height="100%" />
+                    </div>
+                    <div className="card-body d-flex flex-column">
+                      <h5 className="card-title text-center mb-4">
+                        <Skeleton width="60%" />
+                      </h5>
+                      <div className="mt-auto text-center">
+                        <Skeleton width="100px" height="40px" />
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))
-          )}
+              ))
+            : topProducts.map((product: ProductType) => (
+                <div className="col-md-3 mb-4" key={product._id}>
+                  <div className="card h-100 border-0">
+                    <div
+                      className="card-img-top d-flex justify-content-center align-items-center"
+                      style={{
+                        height: "230px",
+                        overflow: "hidden",
+                        position: "relative",
+                      }}
+                    >
+                      <img
+                        src={product.media[0]}
+                        alt={product.title}
+                        className="img-fluid"
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "cover",
+                          borderRadius: "10px",
+                          transition: "transform 0.5s",
+                        }}
+                      />
+                    </div>
+                    <div className="card-body d-flex flex-column">
+                      <h5 className="card-title text-center mb-4">
+                        {product.title}
+                      </h5>
+                      <div className="mt-auto text-center">
+                        <Link
+                          href="/Contact"
+                          className="btn btn-success btn-custom"
+                        >
+                          Contact Us
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+          <div className="bttn">
+            <Link
+              href="/All"
+              className="btn btn-success btn-custom py-2 px-4 rounded"
+            >
+              View All
+            </Link>
+          </div>
         </div>
       </div>
     </section>
