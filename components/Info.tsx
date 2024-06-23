1e3e125feee1af479d2b0d031dc5e3a68e2c33c5
Abhishek Tyagi <abhishektyagi@gmail.com>
2024-06-23 17:58:31 +0530

    phytogenic image chnaged

--- components/Info.tsx
+++ components/Info.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 const Info = () => {
   return (
@@ -7,29 +7,40 @@ const Info = () => {
       <div className="row">
         <div className="col-md-6">
           <div className="plant-content">
-            <p style={{ color: '#214842', fontSize: '32px', fontWeight: '500' }}>
-              Our knowledgeable team can help you design experiences tailored to your needs by accessing one of the flower selections
+            <p
+              style={{ color: "#214842", fontSize: "32px", fontWeight: "500" }}
+            >
+              Camu camu is a Vitamin C power ingredient, it contains 3,000
+              milligrams of vitamin C for every gram of fruit.
             </p>
-            <p style={{ color: '#556260', fontSize: '20px' }}>
-              Elementum eu facilisis sed odio morbi quis commodo odio. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Accumsan sit amet nulla facilisi morbi tempus. Suscipit tellus mauris a diam maecenas sed enim ut sem. Turpis egestas maecenas pharetra convallis posuere
+            <p style={{ color: "#556260", fontSize: "20px" }}>
+              <span style={{font:'bold'}}>SKIN CARE BENEFITS OF CAMU CAMU</span> <br/>
+              • Reduces Pigmentation- Vitamin C
+              and niacin can help fade sun or dark spots by reducing melanin
+              synthesis. <br/>
+              • Smoothing- Vitamin C is used in the synthesis of
+              Collagen. Collagen is an essential protein in skin that provides
+              elasticity, firmness, and youthfulness. <br/> • Protecting- Antioxidants
+              protect our skin from the sun (UVA and UVB rays), pollution and
+              other stressors.<br/> • Brightening- Breaks down dead skin cells
             </p>
           </div>
         </div>
         <div className="col-md-6">
           <div className="herbal-image">
             <Image
-              src="/images/herbal-7.jpg"
+              src="/images/camu.jpg"
               alt="Herbal Image"
               layout="responsive"
               width={500}
               height={500}
-              style={{ border: '1px solid white', borderRadius: '15px' }}
+              style={{ border: "1px solid white", borderRadius: "15px" }}
             />
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Info;
--- components/SomeChange.tsx
+++ components/SomeChange.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const TherapeuticSection = () => {
   return (
@@ -9,32 +9,68 @@ const TherapeuticSection = () => {
         <div className="row">
           <div className="col-md-4 d-flex flex-column justify-content-center align-items-center py-4 ">
             <div className="first-p py-4">
-              <p className="title-p h5 mb-3">Therapeutic Effects of Cannabis and Cannabinoids</p>
+              <p className="title-p h5 mb-3">
+                Kakadu Plum (Terminalia ferdinandiana)
+              </p>
               <p className="content-p mb-4">
-                Accumsan sit amet nulla facilisi morbi tempus. Suscipit tellus mauris a diam maecenas sed enim ut sem.
-                Turpis egestas maecenas pharetra convallis posuere
+                Kakadu Plum (Terminalia ferdinandiana) has 55 times more Vitamin
+                C than an orange. Kakadu plum is usually found as a concentrate
+                or a supplement, and it packs a whopping 150-400 milligrams of
+                Vitamin C per gram.
               </p>
               <Link href="/Contact">
-              <button className="btn btn-success">Contact for info</button>
+                <button className="btn btn-success">Contact for info</button>
               </Link>
             </div>
           </div>
           <div className="col-md-4 d-flex align-items-center justify-content-center">
             <div className="cactus">
-              <Image src="/images/cactus.jpg" alt="Cactus" width={300} height={300} className="img-fluid" />
+              <Image
+                src="/images/kakadu.jpg"
+                alt="Cactus"
+                width={300}
+                height={300}
+                className="img-fluid"
+              />
             </div>
           </div>
           <div className="col-md-4">
             <div className="content-wrapper py-4">
               {[
-                { src: '/images/flash.png', alt: 'Relief of chronic pain', heading: 'Relief of chronic pain', content: 'Feugiat in lorem ipsum fermentum posuere urna nec tincidunt praesent semper feugiat pulvinar proin' },
-                { src: '/images/heart-attack.png', alt: 'Fight cancer', heading: 'Fight cancer', content: 'Feugiat in lorem ipsum fermentum posuere urna nec tincidunt praesent semper feugiat pulvinar proin' },
-                { src: '/images/autism.png', alt: 'Depression', heading: 'Depression', content: 'Feugiat in lorem ipsum fermentum posuere urna nec tincidunt praesent semper feugiat pulvinar proin' },
+                {
+                  src: "/images/flash.png",
+                  alt: "Powerful antioxidant",
+                  heading: "Powerful antioxidant",
+                  content:
+                    "The Kakadu Plum contains even more antioxidants than blueberry, making it one of the most remarkable high-antioxidant foods on the planet.",
+                },
+                {
+                  src: "/images/heart-attack.png",
+                  alt: "Reduces signs of aging",
+                  heading: "Reduces signs of aging",
+                  content:
+                    "Kakadu Plum extract is packed with Vitamins C and E, folic acid and carotenoids, which help reverse the effects of free radicals and reduce the visible signs of aging. It also aids in reducing dark circles and puffiness from under the eyes. ",
+                },
+                {
+                  src: "/images/autism.png",
+                  alt: "Treats acne",
+                  heading: "Treats acne",
+                  content:
+                    "The high content of Vitamin C present in Kakadu Plum makes it an effective remedy for the treatment of acne ",
+                },
               ].map((item, index) => (
                 <div className="content-item d-flex mb-4" key={index}>
-                  <Image src={item.src} alt={item.alt} width={50} height={50} className="img-fluid me-3" />
+                  <Image
+                    src={item.src}
+                    alt={item.alt}
+                    width={50}
+                    height={90}
+                    className="img-fluid me-3"
+                  />
                   <div className="relief-content">
-                    <p className="heading mb-1"><strong>{item.heading}</strong></p>
+                    <p className="heading mb-1">
+                      <strong>{item.heading}</strong>
+                    </p>
                     <p>{item.content}</p>
                   </div>
                 </div>
